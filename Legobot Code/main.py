115d644abea122f4e74955dc9a1604bf34c1e0a9
RockvilleRobotics <l.plazasjones@gmail.com>
2018-07-28 17:11:40 -0700

    Added Legobot Files

---
+++ Legobot Code/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2
+import os
+import sys
+from wallaby import *
+import constants as c
+import actions as a
+import linefollow as f
+import movement as m
+import webcam as w
+import utils as u
+
+def main():
+    print "Starting main()\n"
+    u.setup()
+    u.calibrate()
+    a.get_crates()
+    a.put_crates_in_correct_zone()
+    a.get_botguy()
+    a.put_botguy_on_side()
+    #a.put_botguy_in_correct_zone()
+    u.shutdown()
+
+
+if __name__ == "__main__":
+    sys.stdout = os.fdopen(sys.stdout.fileno(), "w", 0)
+    main()
